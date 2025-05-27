d8315389ab05ae06fc851db29bdccfd839ad330d
kfijalkowski1 <k.1.fijalkowski@gmail.com>
2025-05-27 23:33:34 +0200

    Fix anotations

--- visit_scheduler/app/routers/visits.py
+++ visit_scheduler/app/routers/visits.py
@@ -13,21 +13,21 @@ router = APIRouter(prefix="/visits", tags=["visits"], responses={404: {"descript
 
 @router.get("/all", response_model=None)
 async def get_all_slots(
-    user: UserSessionData = Annotated[UserSessionData, Depends(get_current_user)],
+    user: UserSessionData = Depends(get_current_user),
 ) -> ObjectApiResponse:
     return get_all_time_slots()
 
 
 @router.post("/search", response_model=None)
 async def end_search_vendors(
-    data: SearchVendorModel, user: UserSessionData = Annotated[UserSessionData, Depends(get_current_user)]
+    data: SearchVendorModel, user: UserSessionData = Depends(get_current_user)
 ) -> None:
     return search_vendors(data)
 
 
 @router.post("/add_time_slot", response_model=None)
 async def end_add_time_slot(
-    data: AddTimeSlotModel, user: UserSessionData = Annotated[UserSessionData, Depends(get_current_user)]
+    data: AddTimeSlotModel, user: UserSessionData =  Depends(get_current_user)
 ) -> None:
     return add_time_slot(
         TimeSlotModel(
